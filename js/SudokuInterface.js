1b5ce45e5565f60d9f57dfcd84780bd3f5cde91f
paul rene nichols <paul.rene.nichols@gmail.com>
2013-03-06 16:27:41 -0600

    Starting to build out the SudokuInterface module.  In the process of writing the constructor.

--- js/SudokuInterface.js
+++ js/SudokuInterface.js
@@ -1,3 +1,39 @@
 function SudokuInterface() {
 	
+	/*
+	 * Constructor for the SudokuInterface class
+	 */
+	function SudokuInterface() {
+		
+		this.minimumTileWidth = 20;
+		this.minimumDividerWidth = 1;
+		
+		this.countOfTiles = 9;
+		this.countOfDividers = 10;
+		
+		this.minimumInterfaceWidth = (this.minimumTileWidth * this.countOfTiles) 
+										+ (this.minimumDividerWidth * this.countOfDividers); 
+		
+		if (arguments.length == 0) {		//No arguments provided
+			
+		}
+		else if (arguments.length == 1) {	//The width of the sudoku interface has been provided
+			
+			this.interfaceWidth = arguments[0];
+		}
+		else if (arguments.length == 2) {	//The width of the interface and the SudokuGame object have been given
+			this.interfaceWidth = arguments[0];
+			this.sudokuGame = arguments[1];
+		}
+		
+		if ((typeof this.interfaceWidth != "number") 
+				|| ( isNaN(this.interfaceWidth) )) {
+			throw new Error("Width of Sudoku Interface not provided or NaN");
+		}
+		
+		if (this.interfaceWidth < this.minimumInterfaceWidth) {
+			throw new Error("Specified interface width is insufficient.  Width must be at least " 
+								+ this.minimumInterfaceWidth + " pixels");
+		}
+	};
 };
