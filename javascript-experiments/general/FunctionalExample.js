b94ed16a46e979bd2660b52e5b2851a03c098376
paul rene nichols <paul.rene.nichols@gmail.com>
2013-03-20 13:04:54 -0500

    Added an example of the Functional Constructor pattern

---
+++ javascript-experiments/general/FunctionalExample.js
@@ -0,0 +1,30 @@
+
+var functionalObject = function(initArg1, initArg2) {
+	
+	/*
+	 * private variables.  
+	 * The variable "_that" will be returned by this function
+	 * and will act as the "constructed" object
+	 */
+	var _that = {};  //initialize to empty object
+	var _private1 = initArg1;
+	var _private2 = initArg2;
+	
+	/*
+	 * Public and Private methods can be defined here
+	 */
+	var getP1 = function() { return _private1; };
+	var getP2 = function() { return _private2; };
+	
+	/*
+	 * making methods public involves assigning them to
+	 * the "_that" variable as properties before we return it
+	 */
+	_that.getP1 = getP1;
+	_that.getP2 = getP2;
+	
+	return _that;
+};
+
+var myFO = functionalObject(1, 2);
+var number1 = myFO.getP1();
