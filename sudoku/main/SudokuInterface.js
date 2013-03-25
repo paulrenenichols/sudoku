b12389c3f820da0e60e90d87a3a015aaf5f18847
paul rene nichols <paul.rene.nichols@gmail.com>
2013-03-25 18:05:17 -0500

    Working on adding resizing to sudokuInterface.
    Having trouble figuring out how to resize the Canvas element.  Changing
    height and width attributes of the canvas stops canvas drawing.  Not
    sure why.

--- sudoku/main/SudokuInterface.js
+++ sudoku/main/SudokuInterface.js
@@ -19,30 +19,105 @@ Sudoku.sudokuInterface = function(canvas, sudokuGame) {
 	
 	var _that = {};
 	
-	var _canvas = canvas;
-	var _canvasHeight = _canvas.attr('height');
-	var _canvasWidth = _canvas.attr('width');
+	/*
+	 * _canvas is the JQuery object that holds our
+	 *   canvas element.
+	 *   
+	 * _canvasHeight and _canvasWidth cache the canvas's
+	 *   current height and width in pixels
+	 */
+	var _canvas;
+	var _canvasHeight;
+	var _canvasWidth;
 	
-	var _maxGameBoardWidth = ( _canvasHeight < _canvasWidth ) ? _canvasHeight : _canvasWidth; 
+	/*
+	 * The drawing context for the canvas.
+	 *   This is the object that we use
+	 *   to draw on the canvas.
+	 */
+	var _drawingContext;
+	
+	/*
+	 * _maxGameBoardWidth is the lesser of the canvas's
+	 *   width and height.  This is the upper limit
+	 *   of the size of our game board.
+	 */
+	var _maxGameBoardWidth; 
 
-	var _countSmallDividers = 6;
-	var _countBigDividers = 4;
-	var _countNumberTiles = 9;
+	/*
+	 * Big dividers divide the game board into
+	 *   three sections along each dimension
+	 *   of the board, dividing the board into
+	 *   nine groups of nine number tiles.
+	 *   
+	 * Small dividers divide each of the nine
+	 *   subgroups of number tiles into nine
+	 *   number tiles.
+	 *   
+	 * There are nine number tiles along each
+	 *   dimension of the game board.
+	 */
+	var _countBigDividers;
+	var _countSmallDividers;
+	var _countNumberTiles;
+
+	/*
+	 * These constants set the minimum values for
+	 *   game board dimensions in pixels.
+	 */
+	var _minimumBigDividerWidth;	
+	var _minimumSmallDividerWidth;	
+	var _minimumNumberTileWidth;	
+	var _minimumGameBoardWidth;
 	
-	var _minimumNumberTileWidth = 30;			// in pixels
-	var _minimumBigDividerWidth = 5;	// in pixels
-	var _minimumSmallDividerWidth = 2;	// in pixels
-	var _minimumGameBoardWith = ( _countSmallDividers * _minimumSmallDividerWidth ) +
-								( _countBigDividers * _minimumBigDividerWidth ) +
-								( _countNumberTiles * _minimumNumberTileWidth );
+	/*
+	 * The actual width of the game board
+	 */
+	var _gameBoardWidth;
 	
-	var _numberTileWidth = 60;
-	var _bigDividerWidth = 10;
-	var _smallDividerWidth = 3;
-	var _lineWidth = 598;
+	/*
+	 * Current values for game board dimensions.
+	 */
+	var _numberTileWidth;
+	var _bigDividerWidth;
+	var _smallDividerWidth;
 	
-	
-	var _drawingContext = canvas.getContext("2d");
+	function init(cavnas, sudokuGame) {
+		
+		var minimumWidthMultiplier;
+		var leftOverWidth;
+		var extraTileWidth;
+		
+		_canvas = canvas;
+		_canvasHeight = _canvas.attr('height');
+		_canvasWidth = _canvas.attr('width');
+		
+		_drawingContext = canvas.get(0).getContext("2d");
+		
+		_maxGameBoardWidth = ( _canvasHeight < _canvasWidth ) ? _canvasHeight : _canvasWidth; 
+
+		_countSmallDividers = 6;
+		_countBigDividers = 4;
+		_countNumberTiles = 9;
+		
+		_minimumNumberTileWidth = 30;	// in pixels
+		_minimumBigDividerWidth = 5;	// in pixels
+		_minimumSmallDividerWidth = 2;	// in pixels
+		_minimumGameBoardWidth = ( _countSmallDividers * _minimumSmallDividerWidth ) +
+									( _countBigDividers * _minimumBigDividerWidth ) +
+									( _countNumberTiles * _minimumNumberTileWidth );
+		
+		minimumWidthMultiplier = Math.floor( _maxGameBoardWidth / _minimumGameBoardWidth );
+		leftOverWidth = _maxGameBoardWidth - ( minimumWidthMultiplier * _minimumGameBoardWidth );
+		extraTileWidth = Math.floor( leftOverWidth / _countNumberTiles );
+		
+		_numberTileWidth = ( _minimumNumberTileWidth * minimumWidthMultiplier ) +  extraTileWidth;
+		_bigDividerWidth = _minimumBigDividerWidth * minimumWidthMultiplier;
+		_smallDividerWidth = _minimumSmallDividerWidth * minimumWidthMultiplier;
+		_gameBoardWidth = ( _countSmallDividers * _smallDividerWidth ) +
+							( _countBigDividers * _bigDividerWidth ) +
+							( _countNumberTiles * _numberTileWidth );
+	};
 	
 	function drawHorizontalDividers() {
 		
@@ -51,34 +126,34 @@ Sudoku.sudokuInterface = function(canvas, sudokuGame) {
 		
 		_drawingContext.fillStyle = "#000000";
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _bigDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _bigDividerWidth);
 		yCoordinate += _bigDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _smallDividerWidth);
 		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _smallDividerWidth);
 		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _bigDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _bigDividerWidth);
 		yCoordinate += _bigDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _smallDividerWidth);
 		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _smallDividerWidth);
 		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _bigDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _bigDividerWidth);
 		yCoordinate += _bigDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _smallDividerWidth);
 		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _smallDividerWidth);
 		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
-		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _bigDividerWidth);
+		_drawingContext.fillRect(0, yCoordinate, _gameBoardWidth - 1, _bigDividerWidth);
 	};
 	
 	function drawEmptyGameBoard() {
@@ -101,6 +176,8 @@ Sudoku.sudokuInterface = function(canvas, sudokuGame) {
 	};
 	
 	_that.draw = draw;
+	
+	init(canvas, sudokuGame);
 	
 	return _that;
 };
