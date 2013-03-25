534a95ada9644d126322452bbfede7cc6ea7adbf
paul rene nichols <paul.rene.nichols@gmail.com>
2013-03-25 16:53:17 -0500

    Beginning to add support for resizing.

--- sudoku/main/SudokuInterface.js
+++ sudoku/main/SudokuInterface.js
@@ -11,7 +11,7 @@ var Sudoku = Sudoku ? Sudoku : {};
  * 
  * @class sudokuInterface
  * @constructor 
- * @param canvas {Object} An HTML5 Canvas Element for drawing the interface
+ * @param canvas {Object} A JQuery object whose first element is our canvas
  * @param sudokuGame {Object} An object that stores Sudoku Game State
  * @return {Object} A sudokuInterface object
  */
@@ -19,10 +19,28 @@ Sudoku.sudokuInterface = function(canvas, sudokuGame) {
 	
 	var _that = {};
 	
-	var _tileWidth = 60;
+	var _canvas = canvas;
+	var _canvasHeight = _canvas.attr('height');
+	var _canvasWidth = _canvas.attr('width');
+	
+	var _maxGameBoardWidth = ( _canvasHeight < _canvasWidth ) ? _canvasHeight : _canvasWidth; 
+
+	var _countSmallDividers = 6;
+	var _countBigDividers = 4;
+	var _countNumberTiles = 9;
+	
+	var _minimumNumberTileWidth = 30;			// in pixels
+	var _minimumBigDividerWidth = 5;	// in pixels
+	var _minimumSmallDividerWidth = 2;	// in pixels
+	var _minimumGameBoardWith = ( _countSmallDividers * _minimumSmallDividerWidth ) +
+								( _countBigDividers * _minimumBigDividerWidth ) +
+								( _countNumberTiles * _minimumNumberTileWidth );
+	
+	var _numberTileWidth = 60;
 	var _bigDividerWidth = 10;
 	var _smallDividerWidth = 3;
 	var _lineWidth = 598;
+	
 	
 	var _drawingContext = canvas.getContext("2d");
 	
@@ -34,31 +52,31 @@ Sudoku.sudokuInterface = function(canvas, sudokuGame) {
 		_drawingContext.fillStyle = "#000000";
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _bigDividerWidth);
-		yCoordinate += _bigDividerWidth + _tileWidth;
+		yCoordinate += _bigDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
-		yCoordinate += _smallDividerWidth + _tileWidth;
+		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
-		yCoordinate += _smallDividerWidth + _tileWidth;
+		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _bigDividerWidth);
-		yCoordinate += _bigDividerWidth + _tileWidth;
+		yCoordinate += _bigDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
-		yCoordinate += _smallDividerWidth + _tileWidth;
+		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
-		yCoordinate += _smallDividerWidth + _tileWidth;
+		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _bigDividerWidth);
-		yCoordinate += _bigDividerWidth + _tileWidth;
+		yCoordinate += _bigDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
-		yCoordinate += _smallDividerWidth + _tileWidth;
+		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _smallDividerWidth);
-		yCoordinate += _smallDividerWidth + _tileWidth;
+		yCoordinate += _smallDividerWidth + _numberTileWidth;
 		
 		_drawingContext.fillRect(0, yCoordinate, _lineWidth - 1, _bigDividerWidth);
 	};
