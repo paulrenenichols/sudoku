649faf42e60c5ac2eb9093c717cd6a0609662031
paul rene nichols <paul.rene.nichols@gmail.com>
2013-06-23 13:39:14 -0500

    Fixed useage of require in Sudoku.js
    Used **paths** in the `require.config()` call to simplify the define
    statement.

--- sudoku/js/sudoku/Sudoku.js
+++ sudoku/js/sudoku/Sudoku.js
@@ -5,6 +5,21 @@
 //
 // This is the require.js module for my Sudoku engine
 
-define(['jquery-1.9.1-min'], function($) {
+require.config({
 	
-})
+	paths: {
+		basicMockSudokuGame: 'sudoku/BasicMockSudokuGame',
+		sudokuGame: 'sudoku/SudokuGame',
+		sudokuInterface: 'sudoku/SudokuInterface'
+
+	}
+});
+
+define(['jquery', 'sudokuInterface', 'sudokuGame', 'basicMockSudokuGame'], 
+function($, si, sg, bmsg) {
+	var Sudoku = {};
+	Sudoku.SudokuInterface = si;
+	Sudoku.SudokuGame = sg;
+	Sudoku.BasicMockSudokuGame = bmsg;
+	return Sudoku;
+});
