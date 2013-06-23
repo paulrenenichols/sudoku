2660b51e14ab8c734a31c40d4454bf147cb8a517
paul rene nichols <paul.rene.nichols@gmail.com>
2013-06-23 13:37:27 -0500

    Fixed use of require in main.js
    Used paths to simplify the **require** call.  Fixing this fixed
    **index-require.html**.

--- sudoku/js/main.js
+++ sudoku/js/main.js
@@ -1,14 +1,84 @@
-requirejs.config({
-	baseURL: 'js/lib',
+require.config({
+	baseURL: '/',
 	
 	paths: {
-		sudoku: '../sudoku',
-		test: '../test',
-		utility: '../utility'
+		sudoku: 'sudoku/Sudoku',
+		utility: 'utility/utility',
+		jquery: 'lib/jquery-1.9.1.min',
+		json2: 'lib/json2',
+		modernizr: 'lib/modernizr-2.6.2.min',
+		underscore: 'lib/underscore-min'
+
 	}
 });
 
-requirejs(['jquery-1.9.1.min', 'json2', 'modernizr-2.6.2.min', 'underscore-min', 'SudokuInterface', 'BasicMockSudokuGame'], 
-function($, JSON, Modernizr, _, Sudoku.sudokuInterface, SudokuTest.basicMockSudokuGame) {
+require(['jquery', 'json2', 'underscore', 'sudoku', 'utility', 'modernizr'], 
+function($, JSON, _, Sudoku) {
+	var Modernizr = window.Modernizr;
 	
+	$(document).ready( function() {
+		
+		var sudokuCanvas;
+		var sudokuInterface;
+		var basicMockSudokuGame;
+		var gameBoardWidth;
+		
+		var $canvasWidthRange;
+		var $canvasWidthText;
+		
+		function browserSupportsCanvas() {
+		    return Modernizr.canvas;
+		};
+		
+		if (!browserSupportsCanvas()) {
+			return;
+		};
+		
+		$canvasWidthRange = $('#canvasWidthRange');
+		$canvasWidthText = $('#canvasWidthText');
+		
+		function canvasWidthRangeInputChange(e) {
+			
+			var width = this.value;
+			$canvasWidthText.val(width);
+			
+			console.log("new width: " + width);
+			
+			sudokuCanvas.attr('width', width);
+	        sudokuCanvas.attr('height', width);
+	        
+	        sudokuInterface.resize();
+	        sudokuInterface.draw();
+		};
+		
+	   function canvasWidthTextInputChange(e) {
+	        
+	        var width = parseInt(this.value);
+	        
+	        if (!Utility.isPositiveInteger(width) && !Utility.isZeroNumber(width)) {
+	        	this.value = $canvasWidthRange.val();
+	        	return;
+	        }
+	        
+	        $canvasWidthRange.val(width);
+	        
+	        console.log("new width: " + width);
+	        
+	        sudokuCanvas.attr('width', width);
+	        sudokuCanvas.attr('height', width);
+	        
+	        sudokuInterface.resize();
+	        sudokuInterface.draw();
+	    };
+		
+		sudokuCanvas = $('#sudokuCanvas');
+		
+		basicMockSudokuGame = BasicMockSudokuGame();
+	    sudokuInterface = SudokuInterface(sudokuCanvas, basicMockSudokuGame);
+	    
+	    sudokuInterface.draw();
+
+	    $canvasWidthRange.change(canvasWidthRangeInputChange);
+	    $canvasWidthText.change(canvasWidthTextInputChange);
+	});
 });
