11a5f2caa597f0f9a59c74e9be7c99e5f0421d74
paul rene nichols <paul.rene.nichols@gmail.com>
2013-06-26 22:54:10 -0500

    Created Tile object constructor and TakenTiles object constructor
    This is the first step towards creating the first version of the game
    logic.

--- sudoku/js/sudoku/SudokuGame.js
+++ sudoku/js/sudoku/SudokuGame.js
@@ -7,4 +7,88 @@
 SudokuGame = function() {
 	
 	var _that = {};
+	
+	var _initialGame;
+	
+	var _gameState;
+	
+	var _takenTiles;
+	
+	function Tile(row, column, state) {
+		
+		var _that = {};
+
+		function getRow() {
+			return row;
+		}
+		
+		function getColumn() {
+			return column;
+		}
+		
+		function getState() {
+			return state;
+		}
+		
+		_that.getRow = getRow;
+		_that.getColumn = getColumn;
+		_that.getState = getState;
+		
+		return _that;
+	}
+	
+	function TakenTiles() {
+		
+		var takenTiles = [];
+		
+		var _that = {};
+		
+		function containsTileWithSameCoordinates(tile) {
+			var result = _find(takenTiles, function(currentTile) {
+				return (currentTile.getRow() === tile.getRow()) && (currentTile.getColumn() === tile.getColumn());
+			});
+		}
+		
+		function removeTile(tile) {
+			
+		}
+		
+		function addTile(tile) {
+			takenTiles = _.union(takenTiles, [tile]);
+		}
+		
+	}
+	
+	function create2DSquareArray(width){
+		var array = new Array(width);
+		var i = 0;
+		var j = 0;
+		for(i = 0; i < width; i++) {
+			array[i] = new Array(width);
+			
+			for(j = 0; j < width; j++){
+				array[i][j] = 0;
+			}
+		}
+		
+		return array;
+	}
+	
+	
+	
+	function createNewGame() {
+		
+	}
+	
+	function init() {
+		_initialGame = create2DSquareArray(9);
+		_gameState = create2DSquareArray(9);
+		
+	}
+	
+	_that.Tile = Tile;
+	_that.TakenTiles = TakenTiles;
+	
+	init();
+	return _that;
 };
